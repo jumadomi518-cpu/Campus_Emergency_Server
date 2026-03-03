99d770453c385d13adbaa9102483b153502d0749
Dominic juma <jumadomi518@gmail.com>
2026-03-03 20:34:40 +0300

    c

--- src/routes/emergency.js
+++ src/routes/emergency.js
@@ -14,7 +14,7 @@ function authMiddleware(req, res, next) {
     const token = authHeader.split(" ")[1];
 
     const decoded = jwt.verify(token, process.env.SECRET);
-    console.log("Decoded " + decoded);
+    console.log("Decoded: ", decoded);
     req.user = decoded;
 
     next();
